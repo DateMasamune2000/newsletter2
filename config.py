a757dd30a1c7aeec54babccbb03da291ca84bec8
Varun Girimaji <vgirimaji@gmail.com>
2026-03-01 21:55:24 -0600

    Display works successfully: core functionality implemented

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import json
+import os
 
 def load_config(filename):
     with open(filename, "r") as config_file:
-        return json.loads(config_file.read())
+        obj = json.loads(config_file.read())
+        if obj["cache"]["dir"] == "DEFAULT":
+            obj["cache"]["dir"] = f"{os.getcwd()}/newsletter_cache"
+        return obj
 
--- rss_display.py
+++ rss_display.py
@@ -1,13 +1,20 @@
 import rich
 from lxml import etree
+from config import load_config
 
 def main(argv):
-    demo()
+    config = load_config(argv[0])
 
-def demo():
+    rich.print("[bold blue]NEWSLETTER[/bold blue]")
+    for feed in config["feeds"]:
+        filename = config["cache"]["files"][feed["id"]]
+        path = f"{config['cache']['dir']}/{filename}"
+        pretty_print(path)
+
+def pretty_print(filename):
     feedtree = None
 
-    with open("example_content.xml", "r") as rss_feed:
+    with open(filename, "r") as rss_feed:
         feedtree = etree.fromstring(rss_feed.read().encode())
         for channel in feedtree:
             for channel_child in channel:
@@ -19,6 +26,7 @@ def demo():
                             rich.print(f"\n[white] - [/white] [bold]{item_child.text}[/bold]")
                         elif item_child.tag == "link":
                             rich.print(f"   {item_child.text}")
+        print("\n---\n")
 
 if __name__ == "__main__":
     main()
